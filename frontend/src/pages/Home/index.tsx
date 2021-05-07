7ee800bd093a6800b7179b3d1ef12bdc4d8c9ab0
Pedro Augusto <peedroaugustto@hotmail.com>
2021-05-07 14:30:35 -0300

    Routes

---
+++ frontend/src/pages/Home/index.tsx
@@ -0,0 +1,11 @@
+import ImgDsDark from 'assets/img/ds_dark.svg'
+
+function Home() {
+    return (
+        <div >
+            <h1>pagina home</h1>
+        </div>
+    );
+}
+
+export default Home;
